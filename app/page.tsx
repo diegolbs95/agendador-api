d1efdcf270b83f4930df8689d5b293ee949c9a0c
Diego Luis <diego_bezerra95@hotmail.com>
2026-02-01 11:56:25 -0300

    Adicionando os blocos disponiveis como branco

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       {/* Legenda */}
       <div className="flex gap-4 mb-4 text-gray-900 font-medium">
         <div className="flex items-center gap-1">
-          <div className="w-5 h-5 bg-green-400 rounded"></div>
+          <div className="w-5 h-5 bg-white border border-gray-300 rounded"></div>
           <span className="text-gray-900">Disponível</span>
         </div>
         <div className="flex items-center gap-1">
